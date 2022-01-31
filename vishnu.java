7052533143edc5fdcf93ac908ebb2f4762c28f2c
vishnu pillai <vishnupillai704@gmail.com>
2022-01-31 11:09:39 +0530

    name of the variable changed

--- vishnu.java
+++ vishnu.java
@@ -4,8 +4,9 @@ class test{
              public static void main(String args[])
 {
    Scanner sc=new Scanner(System.in);
+
    int a=sc.nextInt();
-   int b=sc.nextInt();
-   int c=a*b;
+   int d=sc.nextInt();
+   int c=a*d;
    System.out.println("multiplication of a and b is" +c);}
 }
