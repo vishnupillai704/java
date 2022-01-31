1382418624d7ee99f3dbc86b54def0107588cd7b
vishnu pillai <vishnupillai704@gmail.com>
2022-01-31 11:02:49 +0530

    new java file created

---
+++ vishnu.java
@@ -0,0 +1,11 @@
+import Java.util.Scannner;
+class test{
+
+             public static void main(String args[])
+{
+   Scanner sc=new Scanner(System.in);
+   int a=sc.nextInt();
+   int b=sc.nextInt();
+   int c=a*b;
+   System.out.println("multiplication of a and b is" +c);}
+}
